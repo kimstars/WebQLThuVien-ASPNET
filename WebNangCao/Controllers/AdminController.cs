187154f020bac2aad0e400c4261ac64bf9326008
kimstars <buikika9@gmail.com>
2023-12-05 00:04:41 +0700

    Add project files.

---
+++ WebNangCao/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebNangCao.Models;
+
+namespace WebNangCao.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        private AdminEntities adminDb = new AdminEntities();
+
+        [HttpGet]
+        [HandleError]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [HandleError]
+        public ActionResult Login(tblAdmin admin)
+        {
+
+            var adm = adminDb.tblAdmins.SingleOrDefault(a => a.AdminEmail == admin.AdminEmail && a.AdminPass == admin.AdminPass);
+            if (adm != null)
+            {
+                int id = adm.AdminId;
+                Session["adminId"] = adm.AdminId;
+                return RedirectToAction("Index", "TblBooks", new { id = id });
+            }
+            else if (admin.AdminEmail == null && admin.AdminPass == null)
+            {
+                return View();
+            }
+            ViewBag.Message = "Tên đăng nhập hoặc mật khẩu không đúng";
+            return View();
+        }
+       
+        [HandleError]
+        public ActionResult Logout()
+        {
+            Session.Remove("adminId");
+            return RedirectToAction("Home", "Main");
+        }
+    }
+}
---
+++ WebNangCao/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebNangCao.Models;
+
+namespace WebNangCao.Controllers
+{
+    public class UserController : Controller
+    {
+        UserEntity userDb = new UserEntity();
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(tblUser user)
+        {
+            var adm = userDb.tblUsers.SingleOrDefault(a => a.UserEmail == user.UserEmail && a.UserPass == user.UserPass);
+            if (adm != null)
+            {
+                Session["userId"] = adm.UserId;
+                Session["userName"] = adm.UserName;
+                return RedirectToAction("Index", "Borrow", new { userId = adm.UserId, userName = adm.UserName });
+            }
+            else if (user.UserEmail == null && user.UserPass == null)
+            {
+                return View();
+            }
+            ViewBag.Message = "User name and password are not matching";
+            return View();
+        }
+
+        
+        public ActionResult Logout()
+        {
+            Session.Remove("userId");
+            Session.Remove("userName");
+            return RedirectToAction("Home", "Main");
+        }
+    }
+}
